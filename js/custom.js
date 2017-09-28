e536e9a8a6161f40af5337e718da88711b862582
ShaikhAmir <shaikhamir954@gmail.com>
2017-09-28 14:30:49 +0530

    Setup Of Code Done

---
+++ js/custom.js
@@ -0,0 +1,17 @@
+/*Call functions on document ready*/
+$(document).ready(function () {
+
+});
+
+/*Call functions on window load*/
+$(window).load(function () {
+
+});
+
+/* Call function on window scroll */
+$(window).scroll(function () {
+
+});
+
+
+/* Declare all Functions below */
