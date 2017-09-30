7779d4323385ddf85e8722c6801ed2112926191c
anchanadarsh <anchanadarsh@gmail.com>
2017-10-01 03:03:21 +0530

    header and banner

--- _partial/header.php
+++ _partial/header.php
@@ -1,3 +1,30 @@
 <div class="header">
 
+    <nav class="navbar navbar-fixed-top">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+            </div>
+            <div class="collapse navbar-collapse" id="myNavbar">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="index.php">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Blog</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                    <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                    <li><a href="#"><i class="fa fa-pinterest"></i></a></li>
+                    <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
 </div>
---
+++ psd.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>StarBulls</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <!-- Head section included -->
+    <?php include '_partial/head.php' ?>
+</head>
+
+<body>
+    <header>
+        <!-- Header section included -->
+        <?php include '_partial/header.php' ?>
+    </header>
+    <div class="clearfix"></div>
+
+    <main style="min-height: 500px;">
+        <div class="container-fluid p-none">
+            <img style="width:100%" src="ref/securityfinal2.jpg">
+        </div>
+    </main>
+    <div class="clearfix"></div>
+
+    <footer>
+        <!-- Footer section included -->
+        <?php include '_partial/footer.php' ?>
+    </footer>
+</body>
+<!-- Footer scripts included -->
+<?php include '_partial/footer-scripts.php' ?>
+
+</html>
