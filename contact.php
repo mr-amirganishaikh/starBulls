f8f120b6ff5a8d6785160ff3e1ec5b6c717c839a
anchanadarsh <anchanadarsh@gmail.com>
2017-10-01 17:06:59 +0530

    header,footer,samplepages,internal pages

---
+++ contact.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Contact Us | StarBulls</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <!-- Head section included -->
+    <?php include '_partial/head.php' ?>
+</head>
+
+<body>
+    <header>
+        <!-- Header section included -->
+        <?php include '_partial/header.php' ?>
+    </header>
+    <div class="clearfix"></div>
+
+    <main>
+
+
+    </main>
+
+    <div class="clearfix"></div>
+    <footer>
+        <!-- Footer section included -->
+        <?php include '_partial/footer.php' ?>
+    </footer>
+</body>
+<!-- Footer scripts included -->
+<?php include '_partial/footer-scripts.php' ?>
+
+</html>
--- js/custom.js
+++ js/custom.js
@@ -10,8 +10,35 @@ $(window).load(function () {
 
 /* Call function on window scroll */
 $(window).scroll(function () {
-
+    // Add class on scroll
+    fixScrollNav({
+        target: '.navbar',
+        offset: 0
+    });
 });
 
 
 /* Declare all Functions below */
+
+
+// Start Add/remove class on scroll
+var fixScrollNav = function (options) {
+        // Target declaration
+        var target = options.target;
+
+        // offset and offsetHeight declaration
+        var offset = options.offset;
+        var offsetHeight = 0;
+        if (isNaN(offset)) {
+            offsetHeight = $(offset).outerHeight();
+        } else {
+            offsetHeight = offset;
+        }
+
+        if ($(window).scrollTop() > offsetHeight) {
+            $(target).addClass("scrollFixed");
+        } else {
+            $(target).removeClass("scrollFixed");
+        }
+    }
+    // End Add/remove class on scroll
