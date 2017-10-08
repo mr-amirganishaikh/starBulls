c6f4d928df03fe3f691976d7e00833a9f61b5551
adarsh anchan <anchanadarsh@gmail.com>
2017-10-09 01:27:45 +0530

    about us page, page banners

--- about.php
+++ about.php
@@ -16,12 +16,54 @@
         <?php include '_partial/header.php' ?>
     </header>
     <div class="clearfix"></div>
-    
-    <main>
-    
-    
+
+    <div class="page-banner">
+        <img src="img/page-banners/about.jpg" class="img-responsive">
+        <div class="full-black-overlay">
+            <div class="page-name text-center">
+                <h1 class="text-uppercase"><strong>About us</strong> </h1></div>
+        </div>
+        <div class="page-banner-logo">
+            <img src="img/starbullslogo.png" class="img-responsive">
+        </div>
+    </div>
+    <div class="clearfix"></div>
+
+    <main class="m-b-xxl" style="position:relative">
+        <div class="about-vert-divider"></div>
+        <div class="container">
+            <div class="row">
+                <div class="about-box">
+                    <div class="col-xs-offset-3 col-xs-6 about-box-head">
+                        <h3 class="text-uppercase text-center">starbulls security</h3>
+                    </div>
+                    <div class="col-xs-9 about-box-content p-tb-lg">
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
+                    </div>
+                    <div class="col-xs-3 text-center about-box-red">
+                        <div class="vert-cent">
+                            <h1><strong>15</strong><br>YEARS</h1>
+                        </div>
+                    </div>
+                </div>
+                <div class="about-box m-t-xxl">
+                    <div class="col-xs-offset-3 col-xs-6 about-box-head">
+                        <h3 class="text-uppercase text-center">starbulls security</h3>
+                    </div>
+                    <div class="col-xs-9 about-box-content p-tb-lg">
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
+                    </div>
+                    <div class="col-xs-3 text-center about-box-red">
+                        <div class="vert-cent">
+                            <h1><strong>15</strong><br>YEARS</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </main>
-    
+
     <div class="clearfix"></div>
     <footer>
         <!-- Footer section included -->
