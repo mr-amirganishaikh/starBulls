45642eb65f18487d8fa9555a1f1644a572217d36
adarsh anchan <anchanadarsh@gmail.com>
2017-10-16 09:50:13 +0530

    map bg #111

--- contact.php
+++ contact.php
@@ -77,7 +77,7 @@
 
             </div>
         </div>
-        <div class="container-fluid p-none">
+        <div class="container-fluid p-none" style="background:#111">
             <div class="col-xs-12 col-sm-12 p-none">
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3772.670884510116!2d72.83939931451357!3d18.990136987137273!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7cef86c17f29d%3A0xa17a286393a6cd58!2sStarbulls+Facility+Management+Services+(i)+Pvt.Ltd.+Total+Security+Solution!5e0!3m2!1sen!2sin!4v1507831734267" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
             </div>
