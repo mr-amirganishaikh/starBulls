493538c155d6c05d847ce821a2b69ff6ef16df55
adarsh anchan <anchanadarsh@gmail.com>
2018-04-13 00:40:06 +0530

    cds

---
+++ extra-pages/services-back.php
@@ -0,0 +1,289 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Services | StarBulls</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <!-- Head section included -->
+    <?php include '_partial/head.php' ?>
+        <style>
+            .page-banner {
+                background: url(img/page-banners/services.jpg);
+                background-size: cover;
+            }
+        </style>
+</head>
+
+<body>
+    <header>
+        <!-- Header section included -->
+        <?php include '_partial/header.php' ?>
+    </header>
+    <div class="clearfix"></div>
+
+    <div class="page-banner">
+        <div class="full-black-overlay"></div>
+        <div class="page-name text-center">
+            <h1 class="text-uppercase"><strong>services</strong> </h1>
+        </div>
+        <div class="page-banner-logo">
+            <img src="img/starbullslogo.png" class="img-responsive">
+        </div>
+    </div>
+    <div class="clearfix"></div>
+
+    <main class="service-area m-b-xxl">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 m-b-md text-center">
+                    <h3 class="service-category category-active m-lr-sm" set-cat="#category1">Strategic</h3>
+                    <h3 class="service-category m-lr-sm" set-cat="#category2">System</h3>
+                    <h3 class="service-category m-lr-sm" set-cat="#category3">Dog Squad</h3>
+                    <h3 class="service-category m-lr-sm" set-cat="#category4">Fire Security</h3>
+                    <h3 class="service-category m-lr-sm" set-cat="#category5">Transport Escort</h3>
+                </div>
+            </div>
+        </div>
+        <div id="category1" class="service-box-outer" style="display:block">
+            <div class="service-box-inner">
+                <div class="container">
+                    <div class="row" style="overflow:hidden">
+                        <div class="col-xs-12 col-sm-5 p-xl p-lr-xxl service-img-all effect-right">
+                            <div class="service-img">
+                                <img src="img/services/s1.jpg" class="img-responsive">
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-7 p-xl p-lr-xxl service-content effect-left">
+                            <h3>Security Guards</h3>
+                            <p>We offer the best in class security guards. These are men built for the job. Whether it be dealing with your most esteemed customers or guarding your assets. Our men are trained to deal with any challenge that comes their way.</p>
+                            <a href="s-security-guard.php">
+                                <button class="btn btn-starbulls-black">Read More<i class="fa fa-long-arrow-right"></i></button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="service-box-inner">
+                <div class="container">
+                    <div class="row" style="overflow:hidden">
+                        <div class="col-xs-12 col-sm-5 p-xl p-lr-xxl service-img-all effect-right">
+                            <div class="service-img">
+                                <img src="img/services/s2.jpg" class="img-responsive">
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-7 p-xl p-lr-xxl service-content effect-left">
+                            <h3>Security Lady</h3>
+                            <p>Women as a security guard as a lot more to offer in this specialized role. These are women fearless and independent who take a lot of pride in their job for the services the render.They are specifically trained to deal with women in the workplace.</p>
+                            <a href="s-security-lady.php">
+                                <button class="btn btn-starbulls-black">Read More<i class="fa fa-long-arrow-right"></i></button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="service-box-inner">
+                <div class="container">
+                    <div class="row" style="overflow:hidden">
+                        <div class="col-xs-12 col-sm-5 p-xl p-lr-xxl service-img-all effect-right">
+                            <div class="service-img">
+                                <img src="img/services/s3.jpg" class="img-responsive">
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-7 p-xl p-lr-xxl service-content effect-left">
+                            <h3>Security Supervisor/Officer</h3>
+                            <p>As your assets continue to grow and you need your security team to expand. You have come to the right place. Since you have already been your client before and now you need a team to be managed. We have our senior and experienced supervisor cum security officers.</p>
+                            <a href="s-security-supervisor.php">
+                                <button class="btn btn-starbulls-black">Read More<i class="fa fa-long-arrow-right"></i></button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="service-box-inner">
+                <div class="container">
+                    <div class="row" style="overflow:hidden">
+                        <div class="col-xs-12 col-sm-5 p-xl p-lr-xxl service-img-all effect-right">
+                            <div class="service-img">
+                                <img src="img/services/s2.jpg" class="img-responsive">
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-7 p-xl p-lr-xxl service-content effect-left">
+                            <h3>Armed Guards</h3>
+                            <p>We provide Armed guards for your protection. We have been providing security personnel to companies in all parts of India specifically in metropolitan cities.</p>
+                            <a href="s-armed-guard.php">
+                                <button class="btn btn-starbulls-black">Read More<i class="fa fa-long-arrow-right"></i></button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="service-box-inner">
+                <div class="container">
+                    <div class="row" style="overflow:hidden">
+                        <div class="col-xs-12 col-sm-5 p-xl p-lr-xxl service-img-all effect-right">
+                            <div class="service-img">
+                                <img src="img/services/s3.jpg" class="img-responsive">
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-7 p-xl p-lr-xxl service-content effect-left">
+                            <h3>Personal Security Officers / Close Protection Officers</h3>
+                            <p>We have specialized Personal Security officers / Close Protection Officers for your personal protection and your family to protect you from danger: generally theft, assault, kidnapping, assassination, harassment. This protection service is provided anywhere in India.</p>
+                            <a href="s-pso-cpo.php">
+                                <button class="btn btn-starbulls-black">Read More<i class="fa fa-long-arrow-right"></i></button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="category2" class="service-box-outer">
+            <div class="service-box-inner">
+                <div class="container">
+                    <div class="row" style="overflow:hidden">
+                        <div class="col-xs-12 col-sm-5 p-xl p-lr-xxl service-img-all effect-right">
+                            <div class="service-img">
+                                <img src="img/services/s3.jpg" class="img-responsive">
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-7 p-xl p-lr-xxl service-content effect-left">
+                            <h3>CCTV</h3>
+                            <p>You can't continuously keep an eye on your house, assets, office, industry etc 24-hours. At times you need to check your surroundings when your away. For this reason CCTV Cameras are the best answer for you. We at Starbulls have an extensive variety of CCTV camera by which you can screen inside as well as outside of your premises.</p>
+                            <a href="s-cctv.php">
+                                <button class="btn btn-starbulls-black">Read More<i class="fa fa-long-arrow-right"></i></button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="service-box-inner">
+                <div class="container">
+                    <div class="row" style="overflow:hidden">
+                        <div class="col-xs-12 col-sm-5 p-xl p-lr-xxl service-img-all effect-right">
+                            <div class="service-img">
+                                <img src="img/services/s3.jpg" class="img-responsive">
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-7 p-xl p-lr-xxl service-content effect-left">
+                            <h3>Baggage Scanner</h3>
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+                            <a href="s-baggage-scanner.php">
+                                <button class="btn btn-starbulls-black">Read More<i class="fa fa-long-arrow-right"></i></button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <div id="category3" class="service-box-outer">
+            <div class="service-box-inner">
+                <div class="container">
+                    <div class="row" style="overflow:hidden">
+                        <div class="col-xs-12 col-sm-5 p-xl p-lr-xxl service-img-all effect-right">
+                            <div class="service-img">
+                                <img src="img/services/s1.jpg" class="img-responsive">
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-7 p-xl p-lr-xxl service-content effect-left">
+                            <h3>Dog Security</h3>
+                            <p>We provide Detection dogs (sniffer dogs) and Guard dogs. They are well trained and are provided with a verified handler for various offices, grounds, malls, airports and outdoor events. We have dogs from some of the finest breeds in the world. We understand the need for healthy and alert dogs and have trained handlers for the risky situations.</p>
+                            <a href="s-dog-security.php">
+                                <button class="btn btn-starbulls-black">Read More<i class="fa fa-long-arrow-right"></i></button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="service-box-inner">
+                <div class="container">
+                    <div class="row" style="overflow:hidden">
+                        <div class="col-xs-12 col-sm-5 p-xl p-lr-xxl service-img-all effect-right">
+                            <div class="service-img">
+                                <img src="img/services/s3.jpg" class="img-responsive">
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-7 p-xl p-lr-xxl service-content effect-left">
+                            <h3>Dog Handler</h3>
+                            <p>We provide Dog Squad handlers who are security licensed and certified as Security Dog Handlers under the national accreditation standards in India. Our dogs are qualified under strict training and assessment criteria along with their handlers. This helps the dogs build a close rapport with their handlers.</p>
+                            <a href="s-dog-handler.php">
+                                <button class="btn btn-starbulls-black">Read More<i class="fa fa-long-arrow-right"></i></button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div id="category4" class="service-box-outer">
+            <div class="service-box-inner">
+                <div class="container">
+                    <div class="row" style="overflow:hidden">
+                        <div class="col-xs-12 col-sm-5 p-xl p-lr-xxl service-img-all effect-right">
+                            <div class="service-img">
+                                <img src="img/services/s1.jpg" class="img-responsive">
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-7 p-xl p-lr-xxl service-content effect-left">
+                            <h3>Fire Marshal Security</h3>
+                            <p>Starbulls Security Management Services offer a total Fire & Marshal and Security Capability and are your one stop shop for all of your Fire and Security needs. We have well-trained fire-fighter and fire officer to solve all type of fire problem.</p>
+                            <a href="s-fire-marshal.php">
+                                <button class="btn btn-starbulls-black">Read More<i class="fa fa-long-arrow-right"></i></button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div id="category5" class="service-box-outer">
+            <div class="service-box-inner">
+                <div class="container">
+                    <div class="row" style="overflow:hidden">
+                        <div class="col-xs-12 col-sm-5 p-xl p-lr-xxl service-img-all effect-right">
+                            <div class="service-img">
+                                <img src="img/services/s2.jpg" class="img-responsive">
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-7 p-xl p-lr-xxl service-content effect-left">
+                            <h3>Car vehicle Escort</h3>
+                            <p>We provide pilot car drivers who work in conjunction with the driver of an oversize truckload to make sure the truck reaches its destination safely. The pilot car travels in front of or in back of the truck in a separate vehicle, warning the public and the pedestrians if there are traffic changes or obstacles due to the truckload. They also warn the truck if there is anything up ahead that can interfere with its safe passage.</p>
+                            <a href="s-carvehicle-escort.php">
+                                <button class="btn btn-starbulls-black">Read More<i class="fa fa-long-arrow-right"></i></button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="service-box-inner">
+                <div class="container">
+                    <div class="row" style="overflow:hidden">
+                        <div class="col-xs-12 col-sm-5 p-xl p-lr-xxl service-img-all effect-right">
+                            <div class="service-img">
+                                <img src="img/services/s3.jpg" class="img-responsive">
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-7 p-xl p-lr-xxl service-content effect-left">
+                            <h3>PSO & CPO Security Guard Service</h3>
+                            <p>Starbulls Security provides Close Protection Officers (CPOs),Armed Guards, Bodyguards and Personal Security Officers ( PSO’s). We are known for thorough verification and complete background check.</p>
+                            <a href="s-psocpo-officer.php">
+                                <button class="btn btn-starbulls-black">Read More<i class="fa fa-long-arrow-right"></i></button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+
+    <div class="clearfix"></div>
+    <footer>
+        <!-- Footer section included -->
+        <?php include '_partial/footer.php' ?>
+    </footer>
+</body>
+<!-- Footer scripts included -->
+<?php include '_partial/footer-scripts.php' ?>
+
+</html>
